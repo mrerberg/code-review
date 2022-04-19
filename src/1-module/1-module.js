a08fd87c3b42aa511099da3609817292f1faf2f5
Mikhail Cheremuhin-Rerberg <mcheremukhin-rerberg@alfabank.ru>
2022-04-19 20:12:20 +0300

    rename variable with user input

--- src/1-module/1-module.js
+++ src/1-module/1-module.js
@@ -2,31 +2,31 @@ const btn = document.querySelector(".start-game");
 
 
 function getMonthByNumber() {
-  const n = prompt("Введите число от 1-12");
+  const userInput = prompt("Введите число от 1-12");
 
-  if (n == 1) {
+  if (userInput == 1) {
     return "Январь";
-  } else if (n == 2) {
+  } else if (userInput == 2) {
     return "Февраль";
-  } else if (n == 3) {
+  } else if (userInput == 3) {
     return "Март";
-  } else if (n == 4) {
+  } else if (userInput == 4) {
     return "Апрель";
-  } else if (n == 5) {
+  } else if (userInput == 5) {
     return "Май";
-  } else if (n == 6) {
+  } else if (userInput == 6) {
     return "Июнь";
-  } else if (n == 7) {
+  } else if (userInput == 7) {
     return "Июль";
-  } else if (n == 8) {
+  } else if (userInput == 8) {
     return "Август";
-  } else if (n == 9) {
+  } else if (userInput == 9) {
     return "Сентябрь";
-  } else if (n == 10) {
+  } else if (userInput == 10) {
     return "Октябрь";
-  } else if (n == 11) {
+  } else if (userInput == 11) {
     return "Ноябрь";
-  } else if (n == 12) {
+  } else if (userInput == 12) {
     return "Декабрь";
   }
 
